d65708849e1220797b50ae583006e36b445b89f4
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-24 14:46:27 -0600

    Latest clojure and clojurescript.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject spike-mies-2 "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.879"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
