9b71e0ae579ec9b7be93a01b1ef7d060f257d8fd
Michael Whatcott <mdwhatcott@gmail.com>
2021-09-24 14:46:08 -0600

    Implemented "Performing I/O" tutorial.
    https://www.learn-clojurescript.com/section-2/lesson-14-performing-io/

--- src/spike_mies_2/core.cljs
+++ src/spike_mies_2/core.cljs
@@ -1,9 +1,43 @@
 (ns spike-mies-2.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require
+    [goog.dom :as gdom]
+    [goog.events :as gevents]))
 
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+(defn values-same? [field-1 field-2]
+  (= (aget field-1 "value")
+     (aget field-2 "value")))
 
-(enable-console-print!)
+(defn handle-change [password confirmation status]
+  (gdom/setTextContent
+    status
+    (if (values-same? password confirmation)
+      "Matches"
+      "Do not match")))
 
-(println "Hello world!")
+(let [password (gdom/createElement "input")
+      confirm  (gdom/createElement "input")
+      status   (gdom/createElement "p")
+      app      (gdom/getElement "app")]
+
+  (gdom/setProperties password #js {"type" "password"})
+  (gdom/setProperties confirm #js {"type" "password"})
+
+  (gevents/listen password "keyup" #(handle-change password confirm status))
+  (gevents/listen confirm "keyup" #(handle-change password confirm status))
+
+  (gdom/setTextContent app "")
+  (gdom/appendChild app password)
+  (gdom/appendChild app confirm)
+  (gdom/appendChild app status))
+
+
+
+;(ns spike-mies-2.core
+;  (:require [clojure.browser.repl :as repl]))
+;
+;;; (defonce conn
+;;;   (repl/connect "http://localhost:9000/repl"))
+;
+;(enable-console-print!)
+;
+;(println "Hello world!")
